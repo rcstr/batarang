6bfb6c58bf3e40a31c069a94ac31112f5c804f82
Rommel Castro <rcastro@prosoftnearshore.com>
2014-06-01 10:55:18 -0700

    search functionality and new files

--- app/scripts/routes/stocks_search_route.js
+++ app/scripts/routes/stocks_search_route.js
@@ -2,19 +2,29 @@ AcornsTest.StocksSearchRoute = Ember.Route.extend({
   model: function(params) {
     "use strict";
 
-    var stocks = [],
+    var url = AcornsTest.Config.quandl.URL + '.json',
+      stocks = [],
       self = this;
 
-    Ember.$.getJSON(AcornsTest.Config.quandl.URL+'.json', { query: params.symbol, auth_token: AcornsTest.Config.quandl.APIKEY }).then(function(data) {
+    Ember.$.getJSON(url, { query: params.symbol, auth_token: AcornsTest.Config.quandl.APIKEY })
+      .then(function(data) {
       data.docs.forEach(function(doc) {
-        stocks.pushObject(self.store.createRecord('stock', {
-          source_code: doc.source_code,
-          code: doc.code,
-          name: doc.name,
-          description: doc.description,
-          display_url: doc.display_url,
-          source_name: doc.source_name
-        }));
+        var stockID = doc.source_code + doc.code,
+          stockInStore = self.store.getById('stock', stockID);
+
+        if(stockInStore) {
+          stocks.pushObject(stockInStore);
+        } else {
+          stocks.pushObject(self.store.createRecord('stock', {
+            id: stockID,
+            source_code: doc.source_code,
+            code: doc.code,
+            name: doc.name,
+            description: doc.description,
+            display_url: doc.display_url,
+            source_name: doc.source_name
+          }));
+        }
       });
     });
 
